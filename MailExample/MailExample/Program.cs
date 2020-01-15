d6c831670d1643fb61865bf1c6a0cca88a3dfb78
ACT Web Dev <53451641+ACTWebDev@users.noreply.github.com>
2020-01-15 12:25:54 -0700

    Add files via upload

---
+++ MailExample/MailExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace MailExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+            mail.From = new MailAddress("my_email_account@gmail.com");
+            mail.To.Add("someone@hotmail.com");
+            mail.Subject = "C# Mail";
+            mail.Body = "This is for testing SMTP mail from C# application.";
+
+            SmtpServer.Port = 587;
+            SmtpServer.Credentials = new System.Net.NetworkCredential("my_email_account@gmail.com", "PASSWORD1234");
+            SmtpServer.EnableSsl = true;
+
+            SmtpServer.Send(mail);
+        }
+    }
+}
