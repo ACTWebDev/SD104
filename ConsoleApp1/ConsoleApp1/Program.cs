9a2e1477dc3825fe603b0ab6e6969ad4bf3fcdbf
ACT Web Dev <53451641+ACTWebDev@users.noreply.github.com>
2020-01-15 12:03:55 -0700

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+            {
+            Console.WriteLine("Hello World");
+            int myFirstNumber = 19;
+            int mySecondNumber = 244;
+            Console.WriteLine(myFirstNumber * mySecondNumber);
+            string myText = "Some Text";
+            Console.WriteLine(myText + mySecondNumber);
+            string moreText = "Goes Here";
+            Console.WriteLine(myText + moreText);
+            string color1, color2;
+            color1 = "Yellow";
+            color2 = "Red";
+            bool isYellow = true;
+            bool isGreen = false;
+            Console.WriteLine(color1 + isYellow + color2 + isGreen);
+            double numbers;
+            numbers = mySecondNumber / myFirstNumber;
+            Console.WriteLine(numbers);
+            numbers = mySecondNumber / (double)myFirstNumber;
+            Console.WriteLine(numbers);
+            Console.WriteLine("BreakPoint Here");
+
+            string textToNumber = "80473";
+            int results = 99 * int.Parse(textToNumber);
+            Console.WriteLine(results);
+
+            DateTime rightNow = DateTime.Now;
+            Console.WriteLine(rightNow);
+
+
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            string line1 = "myemailaddress@yahoo.com";
+            Console.WriteLine(line1.IndexOf("@"));
+            //@ occurs at character position 14 
+
+            Console.WriteLine(line1.IndexOf("email"));
+            //"email" begins at character position 2 
+
+            Console.WriteLine(line1.IndexOf("Zeus"));
+            //"Zeus" doesn't appear in "myemailaddress@yahoo.com".
+            //Returns -1
+
+
+            string line2 = "The cat in the corner can count.";
+            line2 = line2.Replace("c", "Z");
+            Console.WriteLine(line2);
+            //The Zat in the Zorner Zan Zount.
+
+
+            Console.WriteLine(line2.Length);
+            //32 characters long
+
+            Console.WriteLine(line2.Contains("in the"));
+            //Returns true because the words appear in the sentence.
+
+            //The Zat in the Zorner Zan Zount.
+            string[] words = line2.Split("Z");
+            
+            foreach(string word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+            //The Zat in the Zorner Zan Zount.
+            Console.WriteLine(line2.Substring(4,3));
+            //Returns "Zat", the 5th character, 6th, 7th characters.
+            //Remeber, the 1st character is "0".
+
+            string line3 = "    TRIM ME DOWN       ";
+            Console.WriteLine(line3.Trim());
+            //TRIM ME DOWN
+
+            Console.WriteLine(line3.GetType());
+            //System.String
+
+            string textToSearch = "This is Sparta!";
+            string regex = "[a-z]";
+            MatchCollection matchResults = Regex.Matches(textToSearch, regex);
+
+            foreach (Match m in matchResults)
+            {
+                Console.WriteLine(m);
+            }
+
+            textToSearch = "afgkk24679ggZXVFcatcat9970b02dogMaryxxxxbbbb22FHDFISHMOTHERfaskldjflasjfjasdfjzxcvxcEYERTR345262372546vbcvb0000000carTT2356236623732623462366johnxcxcxvbdfsdgvxcvC#00002499xbvvcfgooglecvcvxzvdfgsdjiiruewortuiwerioptunvxn990348590234523907854034sdnskfshwateririweotowyuwerituwreutpwetuwertueryuwertwerutpweurtpuweptnvmxcvnnxcxcxvb";
+            regex = "[a-z]at";
+
+            matchResults = Regex.Matches(textToSearch, regex);
+
+            foreach (Match m in matchResults)
+            {
+                Console.WriteLine(m);
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
