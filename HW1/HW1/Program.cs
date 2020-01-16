0da4ca8f4510e7c46ebac94ec2dea8e1c72730df
ACT Web Dev <53451641+ACTWebDev@users.noreply.github.com>
2020-01-16 12:01:27 -0700

    Add files via upload

---
+++ HW1/HW1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Solution_2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1; //input one
+            int number2; //input two
+
+            Console.WriteLine("Enter a value for the first number:");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a value for the second number:");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = number1 + number2;
+            Console.WriteLine("The answer is:");
+            Console.WriteLine(result);
+
+            result = number1 - number2;
+            Console.WriteLine("The answer is:");
+            Console.WriteLine(result);
+
+
+            result = number1 * number2;
+            Console.WriteLine("The answer is:");
+            Console.WriteLine(result);
+
+            double results2 = (double)number1 / (double)number2;  //divide 
+            double results3 = number1 % number2;  //reminder (left over after division)
+            Console.WriteLine("The answer is:"); 
+            Console.WriteLine(results2);  //speficy to print the results to screen as a double.
+            Console.WriteLine(results3);
+
+
+
+
+        }
+    }
+}
