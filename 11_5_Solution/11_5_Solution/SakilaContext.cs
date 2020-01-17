4e9c5963d0b5698fc2977beda601fd76194c615e
ACT Web Dev <53451641+ACTWebDev@users.noreply.github.com>
2020-01-17 11:35:21 -0700

    Add files via upload

---
+++ 11_5_Solution/11_5_Solution/SakilaContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+using System.Text;
+
+namespace _11_5_Solution
+{
+    class SakilaContext : DbContext
+    {
+
+        public DbSet<Film> Film { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=localhost\sqlexpress;Database=sakila;Trusted_Connection=True;")
+;        }
+    }
+}
